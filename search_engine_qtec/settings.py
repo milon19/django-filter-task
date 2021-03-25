9778f32bd387368f0986383f18f910297bdef4a0
Milon Mahato <milonmahato67@gmail.com>
2021-03-25 17:14:45 +0600

    Add bootstrap and setup base templates aslo install jquery.

--- search_engine_qtec/settings.py
+++ search_engine_qtec/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_REDIRECT_URL = ''
+LOGIN_URL = ''
