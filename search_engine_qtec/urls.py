d4628bf612a4d86de4aeae31f97b10610da24666
Milon Mahato <milonmahato67@gmail.com>
2021-03-27 01:45:47 +0600

    Authentication task complete with login, logout, register.

--- search_engine_qtec/urls.py
+++ search_engine_qtec/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
+    path('', include('users.urls', namespace='users')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
