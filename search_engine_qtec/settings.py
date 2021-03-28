7dd125d7687cce192c51a18e237493e0492c478e
Milon Mahato <milonmahato67@gmail.com>
2021-03-28 18:25:08 +0600

    Fixed .env file problem.

--- search_engine_qtec/settings.py
+++ search_engine_qtec/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-env.read_env('.env')
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
